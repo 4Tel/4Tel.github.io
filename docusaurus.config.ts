9b7cd204f52b77393bf874b86f96369d6b306737
4tel <kyako95@gmail.com>
2024-12-16 22:42:32 +0900

    small change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
 import {themes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import type {Navbar} from '@docusaurus/theme-common'
+// import type NavBar from '@docusaurus/theme-common/src/utils/useThemeConfig'
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
@@ -30,54 +32,54 @@ const meta: Config = {
   },
 }
 
-/** blog theme */
+/* Top Navigation bar information */
+const navigation = {
+  title: '4Tel',
+  logo: {
+    src: 'img/favicon.ico',
+  },
+  hideOnScroll: true,
+  items: [ {
+      type: 'doc',
+      docId: 'home/home',
+      label: 'Home',
+      position: 'left',
+    }, {
+      type: 'doc',
+      docId: 'study/study',
+      position: 'left',
+      label: 'Study',
+    }, {
+      type: 'doc',
+      docId: 'tuto/tuto',
+      position: 'left',
+      label: 'Tutorial',
+    }, {
+      type: 'doc',
+      docId: 'SCR/SCR',
+      position: 'left',
+      label: 'Starcraft',
+    },
+  ],
+} satisfies Navbar;
+
+/** theme setting */
 const theme = {
-  image: 'img/favicon.ico',
+  image: 'img/favicon.ico', // tab
   colorMode: {
     defaultMode: 'dark',
     disableSwitch: false,
-  },
+  }, prism: codeTheme,
   docs: {
     sidebar: {
       hideable: true,
       autoCollapseCategories: true,
     }
   },
-  navbar: {
-    title: '4Tel',
-    logo: {
-      src: 'img/favicon.ico',
-    },
-    hideOnScroll: true,
-    items: [ {
-        type: 'doc',
-        docId: 'home/home',
-        label: 'Home',
-        position: 'left',
-      }, {
-        type: 'doc',
-        docId: 'study/study',
-        position: 'left',
-        label: 'Study',
-      }, {
-        type: 'doc',
-        docId: 'tuto/tuto',
-        position: 'left',
-        label: 'Tutorial',
-      }, {
-        type: 'doc',
-        docId: 'SCR/SCR',
-        position: 'left',
-        label: 'Starcraft',
-      },
-    ],
-  },
+  navbar: navigation,
   footer: {
     style: 'dark',
     copyright: `Copyright © ${new Date().getFullYear()}. Made by 4Tel.`,
-  },
-  prism: {
-    ...codeTheme
   },
 } satisfies Preset.ThemeConfig
 
@@ -93,38 +95,39 @@ const latex_setting = [
   },
 ]
 
+/* first page */
+const page1 = [
+  'classic',
+  {
+    docs: {
+      sidebarPath: './sidebars.ts',
+      routeBasePath: '/',
+      ...doc_settings,
+    },
+    blog: {
+      showReadingTime: true,
+      feedOptions: {
+        type: ['rss', 'atom'],
+        xslt: true,
+      },
+      ...doc_settings,
+      onInlineTags: 'warn',
+      onInlineAuthors: 'warn',
+      onUntruncatedBlogPosts: 'warn',
+    },
+    theme: {
+      customCss: './src/css/custom.css',
+    },
+    googleAnalytics: {
+      trackingID: 'G-KJQBWD08DH',
+      anonymizeIP: true,
+    }
+  } satisfies Preset.Options,
+]
+
 const config: Config = {
   ...meta,
-    presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          routeBasePath: '/',
-          ...doc_settings,
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          ...doc_settings,
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-        googleAnalytics: {
-          trackingID: 'G-KJQBWD08DH',
-          anonymizeIP: true,
-        }
-      } satisfies Preset.Options,
-    ],
-  ],
+  presets: [page1],
   themeConfig: theme,
   stylesheets: latex_setting,
 };
